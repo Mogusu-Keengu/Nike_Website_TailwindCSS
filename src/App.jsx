d60f37119d6bd289ccf1a10091fab9f7a3106445
Mogusu-Keengu <mogusukeengu@gmail.com>
2026-03-18 08:09:07 +0300

    Added part of Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,46 @@
+import {Hero,
+    PopularProducts,
+    SuperQuality,
+    Services,
+    SpecialOffer,
+    CustomerReview,
+    Subscribe,
+    Footer,
+  } from './sections/index'
+import Nav from './components/Nav'
+
+
+
+
 
 
 const App = () => {
   return (
     <main className="relative">
-      Nav
+      <Nav/>
       <section className="xl:padding-1 wide:padding-r padding-b">
-        Hero
+        <Hero/>
       </section>
       <section className="padding">
-        PopularProducts
+        <PopularProducts/>
       </section>
       <section className="padding">
-        SuperQuality
+        <SuperQuality/>
       </section>
       <section className="padding-x py-10">
-        Services
+        <Services/>
       </section>
       <section className="padding">
-        SpecialOffers
+        <SpecialOffer/>
+      </section>
+      <section className=" bg-pale-blue padding">
+        <CustomerReview/> 
+      </section>
+      <section className="padding-x sm:py-32 py-16 w-full">
+        <Subscribe/>
+      </section>
+      <section className=" bg-black padding-x padding-t pb-8">
+        <Footer/>
       </section>
     </main>
   )
